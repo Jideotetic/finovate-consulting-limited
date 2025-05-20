58a283d9becd56cf2211b2df92cf364e9a22bc4f
Jideotetic <jideotetic@gmail.com>
2025-05-20 10:36:36 +0100

    Added faq section

--- components/faq.tsx
+++ components/faq.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect, RefObject } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 interface FAQItem {
   question: string;
